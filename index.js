4d3423e86b03bb4ee1caad179e80f3c676d6a0c8
Di Wen <ifyouseewendy@gmail.com>
2019-02-17 17:10:17 -0500

    Connect to existed sqlite db*
    https://github.com/chinese-poetry/chinese-poetry/tree/master/ci

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./db/ci.db');
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  db.serialize(function() {
+    db.each("select * from ci limit 1;", function(err, row) {
+      console.log(row);
+      res.send(row);
+    });
+  });
 });
 
 app.listen(3000);
